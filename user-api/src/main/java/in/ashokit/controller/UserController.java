b22a7f3358200d737c5bac6608e6ba6f57940b28
Surendar <batthulasurendar@gmail.com>
2023-11-11 18:44:53 +0530

    controller layer

--- user-api/src/main/java/in/ashokit/controller/UserController.java
+++ user-api/src/main/java/in/ashokit/controller/UserController.java
@@ -1,14 +1,15 @@
 package in.ashokit.controller;
 
+import in.ashokit.request.LoginRequest;
+import in.ashokit.request.PwdChangeRequest;
 import in.ashokit.request.SignUpRequest;
+import in.ashokit.response.LoginResponse;
 import in.ashokit.response.SignUpResponse;
 import in.ashokit.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -31,4 +32,29 @@ public class UserController {
             return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> userLogin (@RequestBody LoginRequest request){
+        LoginResponse response = service.userLogin(request);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/pwd-change")
+    public ResponseEntity<LoginResponse> updatePwd (@RequestBody PwdChangeRequest request){
+        LoginResponse login = service.updatePwd(request);
+        return new ResponseEntity<>(login, HttpStatus.OK);
+    }
+
+    @GetMapping("recover-pwd/{email}")
+    public ResponseEntity<String> recoverPwd (@PathVariable String email){
+        boolean isValid = service.recoverPwd(email);
+        if (isValid) {
+            return new ResponseEntity<>("Password sent to your registratered email", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Invalid Email", HttpStatus.BAD_REQUEST);
+        }
+
+
+    }
+
 }
