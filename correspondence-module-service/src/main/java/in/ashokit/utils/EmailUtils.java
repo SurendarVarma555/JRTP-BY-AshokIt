ec232158f9a4bde0ae665effa7c4846ce41667a1
Surendar <batthulasurendar@gmail.com>
2023-11-22 20:21:09 +0530

    implemented EmailUtils logic

--- correspondence-module-service/src/main/java/in/ashokit/utils/EmailUtils.java
+++ correspondence-module-service/src/main/java/in/ashokit/utils/EmailUtils.java
@@ -1,5 +1,11 @@
 package in.ashokit.utils;
 
+import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -7,12 +13,28 @@ import java.io.File;
 @Component
 public class EmailUtils {
 
+    private static final Logger logger = LoggerFactory.getLogger(EmailUtils.class);
+    @Autowired
+    private JavaMailSender mailSender;
+
     public boolean sendEmail (String to, String subject, String body, File file){
 
         boolean isSent = false;
 
-        // logic
-
+        try {
+            MimeMessage msg = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            if (file != null) {
+                helper.addAttachment(file.getName(), file);
+            }
+            mailSender.send(msg);
+            isSent = true;
+        } catch (Exception e) {
+            logger.error("Error occurred while sending email.", e);
+        }
         return isSent;
 
     }
